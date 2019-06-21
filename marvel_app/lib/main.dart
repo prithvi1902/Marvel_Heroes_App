d358facba3b4d3add8cd39379b36c8949936b60c
prithvi <prithvi@codecraft.co.in>
2019-06-21 12:30:16 +0530

    make favourites

--- marvel_app/lib/main.dart
+++ marvel_app/lib/main.dart
@@ -31,14 +31,20 @@ class FetchHeroes extends State<MarvelState> {
         createdBy: "David Banner",
         publisher: "Marvel Comics",
         imageUrl:
-        "https://imgix.bustle.com/uploads/image/2018/5/17/19fdcf46-db4b-4251-9214-85eb8c48350d-hulk-3.jpg",
+            "https://imgix.bustle.com/uploads/image/2018/5/17/19fdcf46-db4b-4251"
+                "-9214-85eb8c48350d-hulk-3.jpg",
         bio:
-        "\r\n\t\tBruce is a geneticist working with colleague and girlfriend Betty Ross, within the Berkeley Biotechnology Institute on nanomed research to achieve instantaneous cell repair by using low-level gamma radiation exposure to turn on the nanomeds once they are introduced into a living organism, planning to use it to cure all from sicknesses such as cancer and Alzheimer's.\r\n\t\t")
+            "\r\n\t\tBruce is a geneticist working with colleague and girlfriend "
+                "Betty Ross, within the Berkeley Biotechnology Institute on "
+                "nanomed research to achieve instantaneous cell repair by using "
+                "low-level gamma radiation exposure to turn on the nanomeds once "
+                "they are introduced into a living organism, planning to use it "
+                "to cure all from sicknesses such as cancer and Alzheimer's.\r\n\t\t")
   ];
   MarvelHeroesList list = MarvelHeroesList(heroes: marvel); //create an object
 
   final GlobalKey<RefreshIndicatorState> refreshIndicatorKey =
-  GlobalKey<RefreshIndicatorState>();
+      GlobalKey<RefreshIndicatorState>();
 
   Set<Marvel> savedHeroes = Set();
 
@@ -52,8 +58,9 @@ class FetchHeroes extends State<MarvelState> {
 
   @override
   Widget build(BuildContext context) {
-    bool isAlreadySaved = false;
+    bool isAlreadySaved;
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           appBar: AppBar(
             title: Text("Marvel Heroes"),
@@ -66,51 +73,51 @@ class FetchHeroes extends State<MarvelState> {
                   }),
             ],
           ),
-          body: /*RefreshIndicator(
-              key: refreshIndicatorKey,
-              onRefresh: refresh,
-              child: Container(
-                alignment: Alignment.center,
-                child: Container(
-                  alignment: Alignment.center,
-                  child:*/ ListView.builder(itemBuilder: (context, i) {
-                    if (list != null) {
-                      if (i.isOdd) return Divider();
-                      final index = i ~/ 2;
-                      if (index < list.heroes.length) {
-                        Marvel hero = list.heroes[index];
-                        isAlreadySaved = savedHeroes.contains(hero);
-                        return ListTile(
-                          title: Text(hero.name),
-                          subtitle: Text(hero.realName),
-                          leading: Container(
-                            child: FadeInImage.assetNetwork(
-                              placeholder: 'assets/loading.gif',
-                              //specify in .yaml
-                              image: hero.imageUrl,
-                              placeholderScale: 4,
-                            ),
-                          ),
-                          trailing: Icon(
-                            Icons.favorite_border,
-                            color: isAlreadySaved? Colors.red: null,),
-                          onTap: (() {
-                            setState(() {
-                              if (isAlreadySaved) {
-                                savedHeroes.add(hero);
-                              } else {
-                                savedHeroes.remove(hero);
-                              }
-                            });
-                          }),
-                        );
-                      }
-                    } else {
-                      Text("Please wait till we load data!");
-                    }
-                  }/*), //
-                ),
-              )*/)),
+          body: RefreshIndicator(
+            key: refreshIndicatorKey,
+            onRefresh: refresh,
+            child: Container(
+              alignment: Alignment.center,
+              child: ListView.builder(itemBuilder: (context, i) {
+                if (list != null) {
+                  if (i.isOdd) return Divider();
+                  final index = i ~/ 2;
+                  if (index < list.heroes.length) {
+                    Marvel hero = list.heroes[index];
+                    isAlreadySaved = savedHeroes.contains(hero);
+                    return ListTile(
+                      title: Text(hero.name),
+                      subtitle: Text(hero.realName),
+                      leading: Container(
+                        child: FadeInImage.assetNetwork(
+                          placeholder: 'assets/loading.gif',
+                          //specify in .yaml
+                          image: hero.imageUrl,
+                          placeholderScale: 4,
+                        ),
+                      ),
+                      trailing: Icon(
+                        isAlreadySaved ? Icons.favorite : Icons.favorite_border,
+                        color: isAlreadySaved ? Colors.red : null,
+                      ),
+                      onTap: (() {
+                        setState(() {
+                          isAlreadySaved = savedHeroes.contains(hero);
+                          if (isAlreadySaved) {
+                            savedHeroes.remove(hero);
+                          } else {
+                            savedHeroes.add(hero);
+                          }
+                        });
+                      }),
+                    );
+                  }
+                } else {
+                  Text("Please wait till we load data!");
+                }
+              }), //
+            ),
+          )),
     );
   }
 
@@ -124,4 +131,14 @@ class FetchHeroes extends State<MarvelState> {
 class MarvelState extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => FetchHeroes();
+}
+
+class MarvelApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "dbfsjhg",
+      home:MarvelState()
+    );
+  }
 }
